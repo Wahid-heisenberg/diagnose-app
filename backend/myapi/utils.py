2b7be76bbdd60249b6d3e3b43bfcc90e3124519f
wahid-slimani <w_slimani@estin.dz>
2024-04-06 18:20:03 +0100

    created the knowledge base and populated it with diseases and symptomes

--- backend/myapi/utils.py
+++ backend/myapi/utils.py
@@ -17,27 +17,34 @@ for disease in Disease.objects.all():
     for symptom in symptoms:
         kb.tell(expr("HasSymptom('{}', '{}')".format(disease.name, symptom.name)))
 
-# If a patient has four symptoms of a disease, then diagnose the disease
+# If a patient has some symptoms, we can diagnose the disease
 
-kb.tell(expr("""
-forall([x, y, z, w], 
-    implies(and_(HasSymptom(x, y), HasSymptom(x, z), HasSymptom(x, w)), 
-        Diagnose(x))
-)
-"""))
+# Define the rule for diagnosing a disease based on the symptoms
+kb.tell(expr("forall_d(Disease(d) & forall_s(Symptom(s) >> HasSymptom(d, s)), Diagnose(d))"))
 
-# Modify the function to accept symptoms as one string separated by commas
+
+
+
 
 
 def diagnose_disease(symptoms_str):
     # Split the symptoms string into individual symptoms
     symptoms = symptoms_str.split(',')
-    #use the previpus defined logic to diagnose the disease
-    possible_diseases = []
-    for disease in Disease.objects.all():
-        disease_expr = expr("Diagnose('{}')".format(disease.name))
-        if all(bool(kb.ask(expr("HasSymptom('{}', '{}')".format(disease.name, symptom)))) for symptom in symptoms):
-            possible_diseases.append(disease.name)
-    return possible_diseases
+    
+    # Add symptoms to the knowledge base
+    for symptom in symptoms:
+        kb.tell(expr("HasSymptom('{}')".format(symptom)))
+    
+    # Get the disease that the patient has
+    disease = None
+    for d in kb.ask(expr("Diagnose(d)")):
+        disease = d
+    if disease is None:
+        disease = "Unknown"
+
+# Convert the Expr object to a string before returning
+    return str(disease)
+
+
 
 
